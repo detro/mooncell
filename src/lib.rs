a6deb8c9c1b31eba34d761ccdd07a719e246ec27
Ivan De Marino <detronizator@gmail.com>
2018-06-01 23:14:32 +0100

    Organized main lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,12 @@
+#[macro_use] extern crate log;
+extern crate log4rs;
 extern crate serde_json;
 #[macro_use] extern crate serde_derive;
+extern crate tokio;
+extern crate trust_dns_server;
 extern crate reqwest;
 
-pub mod doh_protocol;
+pub mod core;
+pub mod doh;
+pub mod net;
+pub mod logging;
