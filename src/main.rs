77e6e3a2abfb9e963a834dd1bac427abe2718a33
Ivan De Marino <detronizator@gmail.com>
2018-06-01 23:15:08 +0100

    Created main.rs entry to kick start the application

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+#[macro_use]
+extern crate log;
+extern crate mooncell;
+extern crate tokio;
+extern crate trust_dns_server;
+
+use log::LevelFilter;
+use std::{net::{Ipv4Addr, Ipv6Addr}, str::FromStr};
+use tokio::{net::{TcpListener, UdpSocket}};
+use mooncell::{core, net, logging};
+
+fn main() {
+  logging::init(LevelFilter::Debug);
+  info!("DNS Server starting");
+
+  // Establish addresses we will listen on
+  // TODO Make this configurable from CLI
+  let listen_ipv4_addr: Vec<Ipv4Addr> = vec![Ipv4Addr::from_str("0.0.0.0").unwrap()];
+  let listen_ipv6_addr: Vec<Ipv6Addr> = vec![Ipv6Addr::from_str("::").unwrap()];
+  let listen_port: u16 = 53;
+
+  // Binding UDP sockets
+  let udp_sockets: Vec<UdpSocket> = net::utils::bind_udp_sockets(&listen_ipv4_addr, &listen_ipv6_addr, &listen_port);
+  debug!("Bound: {:?}", udp_sockets);
+
+  // Binding TCP sockets
+  let tcp_listeners: Vec<TcpListener> = net::utils::bind_tcp_listeners(&listen_ipv4_addr, &listen_ipv6_addr, &listen_port);
+  debug!("Bound: {:?}", tcp_listeners);
+
+  net::server::listen(udp_sockets, tcp_listeners, core::request_handler::DnsQueryHandler::new());
+}
