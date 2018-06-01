5e044822d9e1bb6059ee438f0e74f41987b40958
Ivan De Marino <detronizator@gmail.com>
2018-06-01 23:11:51 +0100

    Logging module

---
+++ src/logging/mod.rs
@@ -0,0 +1,21 @@
+use log::LevelFilter;
+use log4rs::{
+  self,
+  append::console::ConsoleAppender,
+  config::{Appender, Config as Log4rsConfig, Logger, Root},
+};
+
+pub fn init(log_level_filter: LevelFilter) {
+  // TODO Make the list of modules to filter out a bit more flexible
+  let log_config = Log4rsConfig::builder()
+    .appender(Appender::builder().build("stdout", Box::new(ConsoleAppender::builder().build())))
+    .logger(Logger::builder().build("mio", LevelFilter::Off))
+    .logger(Logger::builder().build("tokio_core", LevelFilter::Off))
+    .logger(Logger::builder().build("tokio_reactor", LevelFilter::Off))
+    .logger(Logger::builder().build("tokio", LevelFilter::Off))
+    .logger(Logger::builder().build("trust_dns_server", LevelFilter::Off))
+    .build(Root::builder().appender("stdout").build(log_level_filter))
+    .unwrap();
+
+  log4rs::init_config(log_config).unwrap();
+}
