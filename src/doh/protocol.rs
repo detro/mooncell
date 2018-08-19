1f158d431628b61947a1862739c8afbef2ae4576
Ivan De Marino <detronizator@gmail.com>
2018-08-19 16:04:31 -0700

    Moving dependency used only in testing, into a test module

--- src/doh/protocol.rs
+++ src/doh/protocol.rs
@@ -1,4 +1,4 @@
-use hyper::{Method, Request, Version};
+use hyper::{Method, Request};
 use serde_json::{self, Error as SerdeJsonError, Value};
 use std::{collections::HashMap, str::FromStr, string::ToString};
 use url::Url;
@@ -128,9 +128,13 @@ pub fn build_request<'a>(provider: &'a DoHProvider, query_name: &str, query_type
 }
 
 // ------------------------------------------------------------------------------------------- Tests
-#[test]
-fn should_deserialize_response() {
-  let dns_resp_json = r#"{
+#[cfg(test)]
+mod test {
+  use super::*;
+  use hyper::Version;
+
+  fn should_deserialize_response() {
+    let dns_resp_json = r#"{
     "Status": 0,
     "TC": false,
     "RD": true,
@@ -167,75 +171,76 @@ fn should_deserialize_response() {
     "edns_client_subnet": "12.34.56.78/0"
   }"#;
 
-  let dns_resp = DoHResponse::from_str(dns_resp_json).unwrap();
+    let dns_resp = DoHResponse::from_str(dns_resp_json).unwrap();
 
-  assert_eq!(dns_resp.status, 0);
-  assert_eq!(dns_resp.truncated, false);
-  assert_eq!(dns_resp.recursion_desired, true);
-  assert_eq!(dns_resp.recursion_available, true);
-  assert_eq!(dns_resp.authenticated_data, false);
-  assert_eq!(dns_resp.checking_disabled, false);
+    assert_eq!(dns_resp.status, 0);
+    assert_eq!(dns_resp.truncated, false);
+    assert_eq!(dns_resp.recursion_desired, true);
+    assert_eq!(dns_resp.recursion_available, true);
+    assert_eq!(dns_resp.authenticated_data, false);
+    assert_eq!(dns_resp.checking_disabled, false);
 
-  assert_eq!(dns_resp.question.len(), 1);
-  assert_eq!(dns_resp.question.get(0).unwrap().name, "apple.com.");
-  assert_eq!(dns_resp.question.get(0).unwrap().question_type, 1);
+    assert_eq!(dns_resp.question.len(), 1);
+    assert_eq!(dns_resp.question.get(0).unwrap().name, "apple.com.");
+    assert_eq!(dns_resp.question.get(0).unwrap().question_type, 1);
 
-  assert_eq!(dns_resp.answer.len(), 3);
-  for answer in &(dns_resp.answer) {
-    assert_eq!(answer.name, "apple.com.");
-    assert_eq!(answer.answer_type, 1);
-    assert_eq!(answer.ttl, 3599);
-    assert!(answer.data.starts_with("17."));
+    assert_eq!(dns_resp.answer.len(), 3);
+    for answer in &(dns_resp.answer) {
+      assert_eq!(answer.name, "apple.com.");
+      assert_eq!(answer.answer_type, 1);
+      assert_eq!(answer.ttl, 3599);
+      assert!(answer.data.starts_with("17."));
+    }
+
+    assert_eq!(dns_resp.additional.len(), 0);
+    assert_eq!(dns_resp.edns_client_subnet, "12.34.56.78/0");
+    assert_eq!(dns_resp.comment, "");
   }
 
-  assert_eq!(dns_resp.additional.len(), 0);
-  assert_eq!(dns_resp.edns_client_subnet, "12.34.56.78/0");
-  assert_eq!(dns_resp.comment, "");
-}
+  #[test]
+  fn should_serialize_response() {
+    let dns_resp_json_orig = r#"{"Status":0,"TC":false,"RD":true,"RA":true,"AD":false,"CD":false,"Question":[{"name":"apple.com.","type":1}],"Answer":[{"name":"apple.com.","type":1,"TTL":3599,"data":"17.178.96.59"},{"name":"apple.com.","type":1,"TTL":3599,"data":"17.172.224.47"},{"name":"apple.com.","type":1,"TTL":3599,"data":"17.142.160.59"}],"Additional":[],"edns_client_subnet":"12.34.56.78/0","Comment":""}"#;
 
-#[test]
-fn should_serialize_response() {
-  let dns_resp_json_orig = r#"{"Status":0,"TC":false,"RD":true,"RA":true,"AD":false,"CD":false,"Question":[{"name":"apple.com.","type":1}],"Answer":[{"name":"apple.com.","type":1,"TTL":3599,"data":"17.178.96.59"},{"name":"apple.com.","type":1,"TTL":3599,"data":"17.172.224.47"},{"name":"apple.com.","type":1,"TTL":3599,"data":"17.142.160.59"}],"Additional":[],"edns_client_subnet":"12.34.56.78/0","Comment":""}"#;
+    let dns_resp = DoHResponse::from_str(dns_resp_json_orig).unwrap();
 
-  let dns_resp = DoHResponse::from_str(dns_resp_json_orig).unwrap();
+    assert_eq!(serde_json::to_string(&dns_resp).unwrap(), dns_resp_json_orig);
+  }
 
-  assert_eq!(serde_json::to_string(&dns_resp).unwrap(), dns_resp_json_orig);
-}
+  #[test]
+  fn should_produce_default_providers() {
+    let def_providers = DoHProvider::defaults();
 
-#[test]
-fn should_produce_default_providers() {
-  let def_providers = DoHProvider::defaults();
+    let google_provider = def_providers.get(DoHProvider::DEFAULT_KEY_GOOGLE).unwrap();
+    assert_eq!(google_provider.host, "dns.google.com");
+    assert_eq!(google_provider.path, "resolve");
+    assert_eq!(google_provider.mandatory_query.len(), 0);
+    assert_eq!(google_provider.url().to_string(), "https://dns.google.com/resolve");
 
-  let google_provider = def_providers.get(DoHProvider::DEFAULT_KEY_GOOGLE).unwrap();
-  assert_eq!(google_provider.host, "dns.google.com");
-  assert_eq!(google_provider.path, "resolve");
-  assert_eq!(google_provider.mandatory_query.len(), 0);
-  assert_eq!(google_provider.url().to_string(), "https://dns.google.com/resolve");
+    let cloudflare_provider = def_providers.get(DoHProvider::DEFAULT_KEY_CLOUDFLARE).unwrap();
+    assert_eq!(cloudflare_provider.host, "cloudflare-dns.com");
+    assert_eq!(cloudflare_provider.path, "dns-query");
+    assert_eq!(cloudflare_provider.mandatory_query.len(), 1);
+    assert_eq!(cloudflare_provider.mandatory_query[0].0, "ct");
+    assert_eq!(cloudflare_provider.mandatory_query[0].1, "application/dns-json");
+    assert_eq!(cloudflare_provider.url().to_string(), "https://cloudflare-dns.com/dns-query?ct=application%2Fdns-json");
+  }
 
-  let cloudflare_provider = def_providers.get(DoHProvider::DEFAULT_KEY_CLOUDFLARE).unwrap();
-  assert_eq!(cloudflare_provider.host, "cloudflare-dns.com");
-  assert_eq!(cloudflare_provider.path, "dns-query");
-  assert_eq!(cloudflare_provider.mandatory_query.len(), 1);
-  assert_eq!(cloudflare_provider.mandatory_query[0].0, "ct");
-  assert_eq!(cloudflare_provider.mandatory_query[0].1, "application/dns-json");
-  assert_eq!(cloudflare_provider.url().to_string(), "https://cloudflare-dns.com/dns-query?ct=application%2Fdns-json");
-}
+  #[test]
+  fn should_create_request() {
+    let default_providers = DoHProvider::defaults();
 
-#[test]
-fn should_create_request() {
-  let default_providers = DoHProvider::defaults();
+    let request = build_request(default_providers.get(DoHProvider::DEFAULT_KEY_GOOGLE).unwrap(), "newrelic", "A");
+    assert_eq!(request.version(), Version::HTTP_11);
+    assert_eq!(request.method(), &Method::GET);
+    assert_eq!(request.uri(), "https://dns.google.com/resolve?name=newrelic&type=A");
+    assert_eq!(request.body(), &());
+    assert!(request.headers().is_empty());
 
-  let request = build_request(default_providers.get(DoHProvider::DEFAULT_KEY_GOOGLE).unwrap(), "newrelic", "A");
-  assert_eq!(request.version(), Version::HTTP_11);
-  assert_eq!(request.method(), &Method::GET);
-  assert_eq!(request.uri(), "https://dns.google.com/resolve?name=newrelic&type=A");
-  assert_eq!(request.body(), &());
-  assert!(request.headers().is_empty());
-
-  let request = build_request(default_providers.get(DoHProvider::DEFAULT_KEY_CLOUDFLARE).unwrap(), "newrelic", "A");
-  assert_eq!(request.version(), Version::HTTP_11);
-  assert_eq!(request.method(), &Method::GET);
-  assert_eq!(request.uri(), "https://cloudflare-dns.com/dns-query?ct=application%2Fdns-json&name=newrelic&type=A");
-  assert_eq!(request.body(), &());
-  assert!(request.headers().is_empty());
+    let request = build_request(default_providers.get(DoHProvider::DEFAULT_KEY_CLOUDFLARE).unwrap(), "newrelic", "A");
+    assert_eq!(request.version(), Version::HTTP_11);
+    assert_eq!(request.method(), &Method::GET);
+    assert_eq!(request.uri(), "https://cloudflare-dns.com/dns-query?ct=application%2Fdns-json&name=newrelic&type=A");
+    assert_eq!(request.body(), &());
+    assert!(request.headers().is_empty());
+  }
 }
