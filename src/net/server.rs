2e25c48cc071e436c2e06002ac7c05266ced9aee
Ivan De Marino <idemarino@newrelic.com>
2018-11-27 21:52:10 +0000

    A stub of a DnsServer
    This will be what we launch at start

---
+++ src/net/server.rs
@@ -0,0 +1,9 @@
+/// The DNS Server that listens for DNS queries over UDP or TCP requests.
+#[derive(Debug, Copy, Clone, Hash)]
+pub struct DnsServer {
+  // TODO
+}
+
+impl DnsServer {
+  // TODO
+}
