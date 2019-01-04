be3d2f61c734f807da3c4868c49aff2204393152
Ivan De Marino <detronizator@gmail.com>
2019-01-04 14:58:17 +0000

    Documenting doh::protocol::DoHResponse

--- src/doh/protocol.rs
+++ src/doh/protocol.rs
@@ -1,7 +1,18 @@
+pub use serde_json::Error as DoHResponseParseError;
+
 use dns::protocol::*;
-use serde_json::{self, Error as SerdeJsonError, Value};
+use serde_json::{self, Value};
 use std::str::FromStr;
 
+/// Represents the deserialized response body for an DNS-over-HTTPS request
+///
+/// The JSON Schema is **not** standardised but major players have decided to play friendly
+/// and keep it consistent. A couple of examples:
+///
+/// * [Cloudflare DNS-over-HTTPS](https://developers.cloudflare.com/1.1.1.1/dns-over-https/json-format/)
+/// * [Google DNS-over-HTTPS](https://developers.google.com/speed/public-dns/docs/dns-over-https)
+///
+/// Hopefully, eventually, there will be a standard document to refer to.
 #[derive(Serialize, Deserialize, Debug)]
 pub struct DoHResponse {
   #[serde(rename = "Status", serialize_with = "dns_response_code_serialize", deserialize_with = "dns_response_code_deserialize")]
@@ -29,18 +40,19 @@ pub struct DoHResponse {
 }
 
 impl FromStr for DoHResponse {
-  type Err = SerdeJsonError;
+  type Err = DoHResponseParseError;
 
-  fn from_str(doh_response_json: &str) -> Result<Self, SerdeJsonError> {
+  fn from_str(doh_response_json: &str) -> Result<Self, Self::Err> {
     Ok(serde_json::from_str(doh_response_json)?)
   }
 }
 
+/// Question part of a `DoHResponse` type
 #[derive(Serialize, Deserialize, Debug)]
 pub struct DoHResponseQuestion {
-  name: String,                             //< FQDN with trailing dot
+  pub name: String,                             //< FQDN with trailing dot
   #[serde(rename = "type", default = "DoHResponseQuestion::question_type_default", serialize_with = "dns_record_type_serialize", deserialize_with = "dns_record_type_deserialize")]
-  question_type: DnsRecordType,             //< Standard DNS RR type (default "A")
+  pub question_type: DnsRecordType,             //< Standard DNS RR type (default "A")
 }
 
 impl DoHResponseQuestion {
@@ -49,14 +61,15 @@ impl DoHResponseQuestion {
   }
 }
 
+/// Answer part of a `DoHResponse` type
 #[derive(Serialize, Deserialize, Debug)]
 pub struct DoHResponseAnswer {
-  name: String,                             //< FQDN with trailing dot
+  pub name: String,                             //< FQDN with trailing dot
   #[serde(rename = "type", default = "DoHResponseAnswer::answer_type_default", serialize_with = "dns_record_type_serialize", deserialize_with = "dns_record_type_deserialize")]
-  answer_type: DnsRecordType,               //< Standard DNS RR type (default "A")
+  pub answer_type: DnsRecordType,               //< Standard DNS RR type (default "A")
   #[serde(rename = "TTL")]
-  ttl: u32,                                 //< Record's time-to-live in seconds
-  data: String,                             //< Data for A - IP address as text
+  pub ttl: u32,                                 //< Record's time-to-live in seconds
+  pub data: String,                             //< Data for A - IP address as text
 }
 
 impl DoHResponseAnswer {
@@ -72,43 +85,43 @@ mod test {
   #[test]
   fn should_deserialize_response() {
     let dns_resp_json = r#"{
-    "Status": 0,
-    "TC": false,
-    "RD": true,
-    "RA": true,
-    "AD": false,
-    "CD": false,
-    "Question": [
-      {
-        "name": "apple.com.",
-        "type": 1
-      }
-    ],
-    "Answer": [
-      {
-        "name": "apple.com.",
-        "type": 1,
-        "TTL": 3599,
-        "data": "17.178.96.59"
-      },
-      {
-        "name": "apple.com.",
-        "type": 1,
-        "TTL": 3599,
-        "data": "17.172.224.47"
-      },
-      {
-        "name": "apple.com.",
-        "type": 1,
-        "TTL": 3599,
-        "data": "17.142.160.59"
-      }
-    ],
-    "Additional": [ ],
-    "edns_client_subnet": "12.34.56.78/0"
-  }"#;
+      "Status": 0,
+      "TC": false,
+      "RD": true,
+      "RA": true,
+      "AD": false,
+      "CD": false,
+      "Question": [
+        {
+          "name": "apple.com.",
+          "type": 1
+        }
+      ],
+      "Answer": [
+        {
+          "name": "apple.com.",
+          "type": 1,
+          "TTL": 3599,
+          "data": "17.178.96.59"
+        },
+        {
+          "name": "apple.com.",
+          "type": 1,
+          "TTL": 3599,
+          "data": "17.172.224.47"
+        },
+        {
+          "name": "apple.com.",
+          "type": 1,
+          "TTL": 3599,
+          "data": "17.142.160.59"
+        }
+      ],
+      "Additional": [ ],
+      "edns_client_subnet": "12.34.56.78/0"
+    }"#;
 
-    let dns_resp = DoHResponse::from_str(dns_resp_json).unwrap();
+    let dns_resp = dns_resp_json.parse::<DoHResponse>().unwrap();
 
     assert_eq!(dns_resp.response_code, DnsResponseCode::NoError);
     assert_eq!(dns_resp.truncated, false);
@@ -138,7 +151,7 @@ mod test {
   fn should_serialize_response() {
     let dns_resp_json_orig = r#"{"Status":0,"TC":false,"RD":true,"RA":true,"AD":false,"CD":false,"Question":[{"name":"apple.com.","type":1}],"Answer":[{"name":"apple.com.","type":1,"TTL":3599,"data":"17.178.96.59"},{"name":"apple.com.","type":1,"TTL":3599,"data":"17.172.224.47"},{"name":"apple.com.","type":1,"TTL":3599,"data":"17.142.160.59"}],"Additional":[],"edns_client_subnet":"12.34.56.78/0","Comment":""}"#;
 
-    let dns_resp = DoHResponse::from_str(dns_resp_json_orig).unwrap();
+    let dns_resp: DoHResponse = dns_resp_json_orig.parse().unwrap();
 
     assert_eq!(serde_json::to_string(&dns_resp).unwrap(), dns_resp_json_orig);
   }
