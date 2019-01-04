8a625177f9684630b00e8155faf5af79cfacca34
Ivan De Marino <detronizator@gmail.com>
2019-01-04 02:26:17 +0000

    Put DoH protocol on its own and made the deserialized DoHResponse more Rusty

---
+++ src/dns/protocol.rs
@@ -0,0 +1,67 @@
+//! Module that re-publishes the part of Trust-DNS Proto that we care about, and adds some utilities
+//! to avoid having to use `trust_dns_proto` in other modules.
+//!
+//! Additionally, it takes care of the "consequences" of the [orphan rule](https://doc.rust-lang.org/book/traits.html#rules-for-implementing-traits)
+//! in regards of **serde**: to be able to use some `trust_dns_proto` types for serialization and
+//! deserialization, a [special workaround](https://serde.rs/remote-derive.html) is needed.
+
+pub use trust_dns_proto::error::{
+  ProtoError as DnsProtoError,
+  ProtoErrorKind as DnsProtoErrorKind,
+};
+pub use trust_dns_proto::{
+  op::{
+    header::MessageType as DnsMessageType,
+    message::Message as DnsMessage,
+    op_code::OpCode as DnsOpCode,
+    response_code::ResponseCode as DnsResponseCode,
+  },
+  rr::{
+    record_type::RecordType as DnsRecordType,
+    dnssec::rdata::DNSSECRecordType as DnsDNSSECRecordType,
+    dns_class::DNSClass as DnsClass,
+  }
+};
+
+use trust_dns_proto::serialize::binary::{BinDecodable, BinEncodable};
+use serde::{ser::Serializer, de::{Deserialize, Deserializer}};
+
+/// Converts an array of raw bytes into a `DnsMessage`
+pub fn dns_message_from_bytes(bytes: &[u8]) -> Result<DnsMessage, DnsProtoError> {
+  DnsMessage::from_bytes(bytes)
+}
+
+/// Converts a `DnsMessage` to a vector of raw bytes
+pub fn dns_message_to_bytes(message: &DnsMessage) -> Result<Vec<u8>, DnsProtoError> {
+  message.to_bytes()
+}
+
+/// Serializes a `DnsResponseCode` into a `u16`
+///
+/// Useful for Serde's `serialize_with` attribute
+pub fn dns_response_code_serialize<S>(response_code: &DnsResponseCode, serializer: S) -> Result<S::Ok, S::Error> where S: Serializer {
+  serializer.serialize_u16(response_code.clone().into())
+}
+
+/// Deserializes a `u16` into a `DnsResponseCode`
+///
+/// Useful for Serde's `deserialize_with` attribute
+pub fn dns_response_code_deserialize<'de, D>(deserializer: D) -> Result<DnsResponseCode, D::Error> where D: Deserializer<'de> {
+  let raw_response_code: u16 = Deserialize::deserialize(deserializer)?;
+  Ok(raw_response_code.into())
+}
+
+/// Serializes a `DnsRecordType` into a `u16`
+///
+/// Useful for Serde's `serialize_with` attribute
+pub fn dns_record_type_serialize<S>(record_type: &DnsRecordType, serializer: S) -> Result<S::Ok, S::Error> where S: Serializer {
+  serializer.serialize_u16(record_type.clone().into())
+}
+
+/// Deserializes a `u16` into a `DnsRecordType`
+///
+/// Useful for Serde's `deserialize_with` attribute
+pub fn dns_record_type_deserialize<'de, D>(deserializer: D) -> Result<DnsRecordType, D::Error> where D: Deserializer<'de> {
+  let raw_record_type: u16 = Deserialize::deserialize(deserializer)?;
+  Ok(raw_record_type.into())
+}
--- src/doh/protocol.rs
+++ src/doh/protocol.rs
@@ -1,64 +1,31 @@
-use hyper::{Method, Request, Body};
+use dns::protocol::*;
 use serde_json::{self, Error as SerdeJsonError, Value};
-use std::{collections::HashMap, str::FromStr, string::ToString};
-use url::Url;
+use std::str::FromStr;
 
-// ----------------------------------------------------------------------------- DoHResponseQuestion
-#[derive(Serialize, Deserialize, Debug)]
-pub struct DoHResponseQuestion {
-  name: String,
-  #[serde(rename = "type", default = "DoHResponseQuestion::question_type_default")]
-  question_type: u16,
-}
-
-impl DoHResponseQuestion {
-  fn question_type_default() -> u16 {
-    1
-  }
-}
-
-// ------------------------------------------------------------------------------- DoHResponseAnswer
-#[derive(Serialize, Deserialize, Debug)]
-pub struct DoHResponseAnswer {
-  name: String,
-  #[serde(rename = "type", default = "DoHResponseAnswer::answer_type_default")]
-  answer_type: u16,
-  #[serde(rename = "TTL")]
-  ttl: u32,
-  data: String,
-}
-
-impl DoHResponseAnswer {
-  fn answer_type_default() -> u16 {
-    1
-  }
-}
-
-// ------------------------------------------------------------------------------------- DoHResponse
 #[derive(Serialize, Deserialize, Debug)]
 pub struct DoHResponse {
-  #[serde(rename = "Status")]
-  status: u32,
+  #[serde(rename = "Status", serialize_with = "dns_response_code_serialize", deserialize_with = "dns_response_code_deserialize")]
+  pub response_code: DnsResponseCode,
   #[serde(rename = "TC")]
-  truncated: bool,
+  pub truncated: bool,                          //< Whether the response is truncated
   #[serde(rename = "RD")]
-  recursion_desired: bool,
+  pub recursion_desired: bool,                  //< Recursion desired
   #[serde(rename = "RA")]
-  recursion_available: bool,
+  pub recursion_available: bool,                //< Recursion available
   #[serde(rename = "AD")]
-  authenticated_data: bool,
+  pub authenticated_data: bool,                 //< Whether all response data was validated with DNSSEC
   #[serde(rename = "CD")]
-  checking_disabled: bool,
+  pub checking_disabled: bool,                  //< Whether the client asked to disable DNSSEC
   #[serde(rename = "Question")]
-  question: Vec<DoHResponseQuestion>,
+  pub question: Vec<DoHResponseQuestion>,       //< See `DoHResponseQuestion` above
   #[serde(rename = "Answer")]
-  answer: Vec<DoHResponseAnswer>,
+  pub answer: Vec<DoHResponseAnswer>,           //< See `DoHResponseAnswer` above
   #[serde(rename = "Additional", default)]
-  additional: Vec<Value>,
+  pub additional: Vec<Value>,
   #[serde(default)]
-  edns_client_subnet: String,
+  pub edns_client_subnet: String,               //< IP address / scope prefix-length
   #[serde(rename = "Comment", default)]
-  comment: String,
+  pub comment: String,
 }
 
 impl FromStr for DoHResponse {
@@ -69,71 +36,40 @@ impl FromStr for DoHResponse {
   }
 }
 
-// ------------------------------------------------------------------------------------- DoHProvider
-pub struct DoHProvider<'a> {
-  method: Method,
-  host: &'a str,
-  path: &'a str,
-  mandatory_query: Vec<(&'a str, &'a str)>,
+#[derive(Serialize, Deserialize, Debug)]
+pub struct DoHResponseQuestion {
+  name: String,                             //< FQDN with trailing dot
+  #[serde(rename = "type", default = "DoHResponseQuestion::question_type_default", serialize_with = "dns_record_type_serialize", deserialize_with = "dns_record_type_deserialize")]
+  question_type: DnsRecordType,             //< Standard DNS RR type (default "A")
 }
 
-impl<'a> DoHProvider<'a> {
-  pub const DEFAULT_KEY_GOOGLE: &'static str = "google";
-  pub const DEFAULT_KEY_CLOUDFLARE: &'static str = "cloudflare";
-
-  pub fn defaults<'b>() -> HashMap<&'b str, DoHProvider<'b>> {
-    let mut providers = HashMap::new();
-
-    providers.insert(Self::DEFAULT_KEY_GOOGLE, DoHProvider {
-      method: Method::GET,
-      host: "dns.google.com",
-      path: "resolve",
-      mandatory_query: vec![],
-    });
-    providers.insert(Self::DEFAULT_KEY_CLOUDFLARE, DoHProvider {
-      method: Method::GET,
-      host: "cloudflare-dns.com",
-      path: "dns-query",
-      mandatory_query: vec![("ct", "application/dns-json")],
-    });
-
-    providers
-  }
-
-  pub fn url(&self) -> Url {
-    let mut url = Url::parse(&format!("https://{}", self.host)).unwrap();
-    url.set_path(self.path);
-
-    for query_param in self.mandatory_query.iter() {
-      url.query_pairs_mut().append_pair(query_param.0, query_param.1);
-    }
-
-    url
+impl DoHResponseQuestion {
+  fn question_type_default() -> DnsRecordType {
+    DnsRecordType::A
   }
 }
 
-// -------------------------------------------------------------------------------- Request building
-pub fn build_request<'a>(provider: &'a DoHProvider, query_name: &str, query_type: &str) -> Request<Body> {
-  // Append to the URL required by the Provider, the query parameters
-  let mut url = provider.url();
-  url.query_pairs_mut()
-    .append_pair("name", query_name)
-    .append_pair("type", query_type);
-
-  // Assemble the final request
-  Request::builder()
-    .method(provider.method.clone())
-    .uri(url.to_string())
-    .body(Body::empty())
-    .unwrap()
+#[derive(Serialize, Deserialize, Debug)]
+pub struct DoHResponseAnswer {
+  name: String,                             //< FQDN with trailing dot
+  #[serde(rename = "type", default = "DoHResponseAnswer::answer_type_default", serialize_with = "dns_record_type_serialize", deserialize_with = "dns_record_type_deserialize")]
+  answer_type: DnsRecordType,               //< Standard DNS RR type (default "A")
+  #[serde(rename = "TTL")]
+  ttl: u32,                                 //< Record's time-to-live in seconds
+  data: String,                             //< Data for A - IP address as text
 }
 
-// ------------------------------------------------------------------------------------------- Tests
+impl DoHResponseAnswer {
+  fn answer_type_default() -> DnsRecordType {
+    DnsRecordType::A
+  }
+}
+
 #[cfg(test)]
 mod test {
   use super::*;
-  use hyper::Version;
 
+  #[test]
   fn should_deserialize_response() {
     let dns_resp_json = r#"{
     "Status": 0,
@@ -174,7 +110,7 @@ mod test {
 
     let dns_resp = DoHResponse::from_str(dns_resp_json).unwrap();
 
-    assert_eq!(dns_resp.status, 0);
+    assert_eq!(dns_resp.response_code, DnsResponseCode::NoError);
     assert_eq!(dns_resp.truncated, false);
     assert_eq!(dns_resp.recursion_desired, true);
     assert_eq!(dns_resp.recursion_available, true);
@@ -183,12 +119,12 @@ mod test {
 
     assert_eq!(dns_resp.question.len(), 1);
     assert_eq!(dns_resp.question.get(0).unwrap().name, "apple.com.");
-    assert_eq!(dns_resp.question.get(0).unwrap().question_type, 1);
+    assert_eq!(dns_resp.question.get(0).unwrap().question_type, DnsRecordType::A);
 
     assert_eq!(dns_resp.answer.len(), 3);
     for answer in &(dns_resp.answer) {
       assert_eq!(answer.name, "apple.com.");
-      assert_eq!(answer.answer_type, 1);
+      assert_eq!(answer.answer_type, DnsRecordType::A);
       assert_eq!(answer.ttl, 3599);
       assert!(answer.data.starts_with("17."));
     }
@@ -207,41 +143,4 @@ mod test {
     assert_eq!(serde_json::to_string(&dns_resp).unwrap(), dns_resp_json_orig);
   }
 
-  #[test]
-  fn should_produce_default_providers() {
-    let def_providers = DoHProvider::defaults();
-
-    let google_provider = def_providers.get(DoHProvider::DEFAULT_KEY_GOOGLE).unwrap();
-    assert_eq!(google_provider.host, "dns.google.com");
-    assert_eq!(google_provider.path, "resolve");
-    assert_eq!(google_provider.mandatory_query.len(), 0);
-    assert_eq!(google_provider.url().to_string(), "https://dns.google.com/resolve");
-
-    let cloudflare_provider = def_providers.get(DoHProvider::DEFAULT_KEY_CLOUDFLARE).unwrap();
-    assert_eq!(cloudflare_provider.host, "cloudflare-dns.com");
-    assert_eq!(cloudflare_provider.path, "dns-query");
-    assert_eq!(cloudflare_provider.mandatory_query.len(), 1);
-    assert_eq!(cloudflare_provider.mandatory_query[0].0, "ct");
-    assert_eq!(cloudflare_provider.mandatory_query[0].1, "application/dns-json");
-    assert_eq!(cloudflare_provider.url().to_string(), "https://cloudflare-dns.com/dns-query?ct=application%2Fdns-json");
-  }
-
-  #[test]
-  fn should_create_request() {
-    let default_providers = DoHProvider::defaults();
-
-    let request = build_request(default_providers.get(DoHProvider::DEFAULT_KEY_GOOGLE).unwrap(), "newrelic", "A");
-    assert_eq!(request.version(), Version::HTTP_11);
-    assert_eq!(request.method(), &Method::GET);
-    assert_eq!(request.uri(), "https://dns.google.com/resolve?name=newrelic&type=A");
-    assert_eq!(request.body(), &());
-    assert!(request.headers().is_empty());
-
-    let request = build_request(default_providers.get(DoHProvider::DEFAULT_KEY_CLOUDFLARE).unwrap(), "newrelic", "A");
-    assert_eq!(request.version(), Version::HTTP_11);
-    assert_eq!(request.method(), &Method::GET);
-    assert_eq!(request.uri(), "https://cloudflare-dns.com/dns-query?ct=application%2Fdns-json&name=newrelic&type=A");
-    assert_eq!(request.body(), &());
-    assert!(request.headers().is_empty());
-  }
 }
