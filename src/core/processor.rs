8091aa31022dac5fe1007081908ec4da78cb25f6
Ivan De Marino <detronizator@gmail.com>
2019-01-06 22:07:32 +0000

    Processor stub made to compile

--- src/core/processor.rs
+++ src/core/processor.rs
@@ -1,8 +1,6 @@
 use std::sync::mpsc::Receiver;
 
-use dns_proto::DnsMessage;
 use net::request_responder::DnsRequestResponder;
-use doh::protocol::DoHResponse;
 
 pub struct Processor {
   receiver: Receiver<DnsRequestResponder>,
