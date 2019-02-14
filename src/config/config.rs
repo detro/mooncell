7e59688a0c6a74b637d00151a1a890f94b34574b
Ivan De Marino <detronizator@gmail.com>
2019-02-14 20:41:54 +0000

    Implemented Config.resolver(): the remainder of the application doesn't have to know how that Resolution actually works

--- src/config/config.rs
+++ src/config/config.rs
@@ -2,8 +2,8 @@
 
 use log::LevelFilter;
 use std::net::{Ipv4Addr, Ipv6Addr};
-use core::protocol::DoHProtocol;
-use core::provider::DoHProvider;
+use core::{protocol::DoHProtocol, provider::DoHProvider, resolver::DoHResolver};
+use doh_json::{resolver::DoHJsonResolver, provider::DoHJsonProvider};
 
 /// This trait is implemented by types that _provide configuration_ to the rest of the application.
 pub trait Config {
@@ -24,4 +24,16 @@ pub trait Config {
 
   /// The DNS-over-HTTPS Provider to use
   fn provider(&self) -> Option<Box<dyn DoHProvider>>;
+
+  /// The DNS-over-HTTPS Resolver to use
+  fn resolver(&self) -> Box<dyn DoHResolver> {
+    match self.protocol() {
+      DoHProtocol::JSON => match self.provider() {
+        Some(provider) => Box::new(DoHJsonResolver::new(*provider.downcast::<DoHJsonProvider>().unwrap())),
+        None => panic!("Unable to determine DoH JSON Provider: this should never be reached!"),
+      },
+      // TODO Rewrite the following once support for WIRE protocol is implemented
+      DoHProtocol::WIRE => panic!("Unable to determine DoH WIRE Provider: this should never be reached!"),
+    }
+  }
 }
