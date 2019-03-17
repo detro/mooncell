2160d68283c78d1f13cbd1407dee9b95c5e2d41f
Ivan De Marino <detronizator@gmail.com>
2019-03-17 16:08:07 +0100

    WIP processor

--- src/core/processor.rs
+++ src/core/processor.rs
@@ -1,19 +1,39 @@
 //! Processor...
 
-use std::sync::mpsc::Receiver;
 use net::request::Request;
 use core::resolver::DoHResolver;
+use threadpool::{Builder as ThreadPoolBuilder, ThreadPool};
+use num_cpus;
+use std::sync::{atomic::{AtomicBool, Ordering}, mpsc::Receiver};
 
 pub struct Processor {
   receiver: Receiver<Request>,
   resolver: Box<dyn DoHResolver>,
+  pool: ThreadPool,
+  running: AtomicBool
 }
 
 impl Processor {
   pub fn new(receiver: Receiver<Request>, resolver: Box<dyn DoHResolver>) -> Processor {
+    let pool = ThreadPoolBuilder::new()
+      .num_threads(num_cpus::get() * 4)
+      .thread_name("Processor Thread Pool".into())
+      .build();
+
     Processor {
       receiver,
-      resolver
+      resolver,
+      pool,
+      running: AtomicBool::new(false)
     }
+  }
+
+  pub fn start(&self) {
+
+  }
+
+  pub fn stop(&self) {
+    self.running.swap(false, Ordering::Relaxed);
+    self.pool.join();
   }
 }
